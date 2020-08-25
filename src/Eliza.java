8b57aa69917884b05184f96092a4affae62df6e3
zanderhorstman <zanderhorstman@gmail.com>
2020-08-25 08:42:43 -0400

    4

--- src/Eliza.java
+++ src/Eliza.java
@@ -15,10 +15,8 @@ public class Eliza {
         System.out.println("Okay, well did anything special or unexpected happen today?");
         special = keybd.nextLine();
         System.out.print("Hmmm... well '" + special);
-        System.out.println("' is fine.");
-        System.out.print("Oh, dang I'm outta time! Talk to ya next time, " + name);
-        System.out.println(".");
-        System.out.println("This conversation has been terminated.\n\nYour Responses were as follows, in this order:\n" + name );
+        System.out.println("' is fine.\nOh, dang I'm outta time! Talk to ya next time, " + name);
+        System.out.println(".\nThis conversation has been terminated.\n\nYour Responses were as follows, in this order:\n" + name );
         System.out.println(""+ day);
         System.out.println(""+ special);
         System.out.println("\n\nPlease Come chat with ZBot again! I don't say much, but I'm oiginal!");
