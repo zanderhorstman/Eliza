9feedbc8d066a80409b3eb0859a7483efb005477
zanderhorstman <zanderhorstman@gmail.com>
2020-08-25 11:20:18 -0400

    8

--- src/Eliza.java
+++ src/Eliza.java
@@ -3,23 +3,13 @@ import java.util.Scanner;
 public class Eliza {
     public static void main(String[] args) {
         Scanner keybd = new Scanner(System.in);
-        String name;
-        String day;
-        String special;
-
-        System.out.println("Welcome to ZBot! My name is Z, who am I speaking with?");
-        name = keybd.nextLine();
-        System.out.print("It's nice to meet you, " + name);
-        System.out.println(", how are ya today?");
-        day = keybd.nextLine();
-        System.out.println("Okay, well did anything special or unexpected happen today?");
-        special = keybd.nextLine();
-        System.out.print("Hmmm... well '" + special);
-        System.out.print("' is fine.\nOh, dang I'm outta time! Talk to ya next time, " + name);
-        System.out.println(".\nThis conversation has been terminated.\n\nYour Responses were as follows, in this order:\n" + name );
-        System.out.println(""+ day);
-        System.out.println(""+ special);
-        System.out.println("\n\nPlease come chat with ZBot again! I don't say much, but I'm original!");
-
+        System.out.println("Welcome to ElizaChats! My name is Eliza, who am I speaking with?");
+        String name = keybd.nextLine();
+        System.out.println("It's nice to meet you, " + name + ", how are you today?");
+        String day = keybd.nextLine();
+        System.out.println("Okay, what do you think is making you feel " + day + "?");
+        String special = keybd.nextLine();
+        String allAnswers = ("\n" + name + "\n" + day + "\n" + special);
+        System.out.println("Well I gotta go! My time is up. See ya next time, " + name + "!\nYour answers were as follows:" + allAnswers);
     }
 }
